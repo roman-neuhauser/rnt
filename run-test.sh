6bdea110ebca5b44531d085bea6e733f60627b1b
Roman Neuhauser <rneuhauser@suse.cz>
2012-07-17 14:30:16 +0200

    run-tests.sh, run-test.sh from logdemux
    taken from logdemux @ 20d63c4fe95a (not published yet).

---
+++ run-test.sh
@@ -0,0 +1,25 @@
+# Copyright (c) Roman Neuhauser
+# Distributed under the MIT license (see LICENSE file)
+# vim: sw=2 sts=2 ts=2 et
+
+curdir="$PWD"
+testdir="$1"; shift
+rm -f "$testdir"/*.actual
+cd "$testdir" || exit 1
+$SHELL ./cmd ${1+"$@"} >out.actual 2>err.actual
+ex=$?
+cd "$curdir"
+echo $ex > "$testdir"/exit.actual
+ex=0
+for exp in $(find "$testdir" -name \*.expected | sort); do
+  act="${exp%.expected}.actual"
+  diff="${exp%.expected}.diff"
+  diff -Nu --strip-trailing-cr "$exp" "$act" > "$diff"
+  dex=$?
+  if test 0 -ne $dex; then
+    ex=$dex
+  else
+    rm -f "$diff"
+  fi
+done
+exit $ex
---
+++ run-tests.sh
@@ -0,0 +1,35 @@
+# Copyright (c) Roman Neuhauser
+# Distributed under the MIT license (see LICENSE file)
+# vim: sw=2 sts=2 ts=2 et
+
+testdir="${1:?}"; shift
+
+cnt=0
+ex=0
+failures=
+
+for d in "$testdir"/???-*; do
+  cnt=$((cnt + 1))
+  "$SHELL" run-test.sh "$d" ${1+"$@"}
+  tex=$?
+  if test 0 -eq $tex; then
+    printf .
+  else
+    ex=$tex
+    printf F
+    failures="$failures $d"
+  fi
+done
+
+printf "\n\n"
+
+fcnt=0
+for f in $failures; do
+  fcnt=$(($fcnt + 1))
+  printf "FAIL $f\n\n"
+  find $f -name \*.diff | xargs cat
+  printf "\n"
+done
+
+printf "tests: %d failed: %d\n" $cnt $fcnt
+exit $ex

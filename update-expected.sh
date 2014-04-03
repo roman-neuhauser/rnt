7ca71d8d67ab535d69ddf7c5ccdc46294b9377df
Roman Neuhauser <rneuhauser@suse.cz>
2014-04-03 11:41:59 +0200

    update-expected.sh: reacts to -h|--help

--- update-expected.sh
+++ update-expected.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+### HELP START # {{{
+# usage: $0 <path>...
+#
 # each argument is one of:
 # * an *.actual, or an *.expected, or a *.diff file path
 #   - this will update the corresponding *.expected file
@@ -7,6 +10,23 @@
 # * a test directory path
 #   - this will update all *.expected files in the directory
 #     from their corresponding *.actual files
+### HELP END # }}}
+
+print_section() # {{{
+{
+  local -r s="${1?}" self=${0##*/}
+  sed -rnf - ${0} <<EOSED
+    /^### $s START/,/^### $s END/ {
+      s:\\\$0:$self:
+      s:^#( |$)::p
+    }
+EOSED
+} # }}}
+
+if [ $# -eq 0 ] || [ "x$1" = x--help ] || [ "x$1" = x-h ]; then
+  print_section HELP
+  exit
+fi
 
 for arg in "$@"; do
   case "$arg" in

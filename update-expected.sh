a9e2934e058bbf7621f02687e58fd7ceb3c9abff
Roman Neuhauser <rneuhauser@suse.cz>
2014-04-03 11:17:23 +0200

    update-expected.sh: better english, info in docstring

--- update-expected.sh
+++ update-expected.sh
@@ -2,11 +2,11 @@
 
 # each argument is one of:
 # * an *.actual, or an *.expected, or a *.diff file path
-#   - this will update the corresponding *.expected file into
+#   - this will update the corresponding *.expected file
 #     from its *.actual file
 # * a test directory path
-#   - this will update all *.expected files under the directory
-#     from their *.actual ones
+#   - this will update all *.expected files in the directory
+#     from their corresponding *.actual files
 
 for arg in "$@"; do
   case "$arg" in
